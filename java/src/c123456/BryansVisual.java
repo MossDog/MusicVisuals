e370b54d5996c0fe13729067b4abd50436b5282f
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-03-18 20:16:06 +0000

    Day 1 - Setup TV Display

--- java/src/c123456/BryansVisual.java
+++ java/src/c123456/BryansVisual.java
@@ -1,94 +1,76 @@
 package c123456;
 
 import ie.tudublin.Visual;
-import ie.tudublin.VisualException;
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
-import processing.core.PApplet;
+//import ie.tudublin.VisualException;
 
-public class BryansVisual extends Visual {
-
+public class BryansVisual extends Visual 
+{
 
     public void settings()
     {
-        size(800, 800, P3D);
-        println("CWD: " + System.getProperty("user.dir"));
-        //fullScreen(P3D, SPAN);
-    }
+        size(500, 500, P3D);//min size for tv graphic is 500 x 500
+        println("CWD: " + System.getProperty("user.dir"));//current working directory
+        fullScreen(P3D,SPAN);// full screen
+    }//end settings
 
     public void keyPressed()
     {
-        if (key == ' ')
+        if (key == ' ')//spacebar
         {
             getAudioPlayer().cue(0);
             getAudioPlayer().play();
-            
-        }
- 
-    }
+        }//end if statement
+    }//end keyPressed
 
     public void setup()
     {
-        colorMode(HSB);
+        colorMode(RGB);
         noCursor();
-        
         setFrameSize(256);
-
         startMinim();
-        loadAudio("heroplanet.mp3");
+        loadAudio("rapgod.mp3");
         getAudioPlayer().play();
         //startListening(); 
-        
-    }
-
-    float radius = 200;
-
-    float smoothedBoxSize = 0;
-
-    float rot = 0;
+    }//end setup
 
     public void draw()
     {
-        calculateAverageAmplitude();
-        try
+        //background
+        background(100,100,100);
+        noStroke();
+        //border calculation
+        float borderx = width * 0.2f;
+        float bordery = height * 0.25f;
+        float frame = 30f;
+        float detail = 5f;
+        //details --> minimum size for tv details
+        if(height >= 500 && width >= 500)
         {
-            calculateFFT();
-        }
-        catch(VisualException e)
-        {
-            e.printStackTrace();
-        }
-        calculateFrequencyBands();
-        background(0);
-        noFill();
-        stroke(255);
-        lights();
-        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        camera(0, -500, 500, 0, 0, 0, 0, 1, 0);
-        //translate(0, 0, -250);
+            //tv frame
+            fill(150); 
+            rect(borderx-frame, bordery-frame, width-(borderx*2)+(frame*2), height-(bordery*2)+(frame*4), detail*2);    
+            //details
+            fill(100);
+            rect(borderx, (bordery + height-(bordery*2)) + detail, width-(borderx*2), detail, detail*2);//small thing indent
+            strokeWeight(2);
+            stroke(95);
+            rect(borderx, (bordery + height-(bordery*2)) + detail*3, width-(borderx*2), frame*2, detail*2);//panel
+            noStroke();
+            //details --> input
+            fill(60);
+            rect(borderx + frame, (bordery + height-(bordery*2)) + (detail*3) + (frame / 2), frame*2, frame, detail);//power button
+            rect(borderx + (frame*(float)2.5) + frame, (bordery + height-(bordery*2)) + (detail*3) + (frame / 2), frame - (frame/3), frame, detail);//channel button prev
+            rect(borderx + (frame*(float)2.5) + frame*2, (bordery + height-(bordery*2)) + (detail*3) + (frame / 2), frame - (frame/3), frame, detail);//channel button prev
+            strokeWeight(2);//proper stroke weight
+            stroke(92, 145, 230);//light blue
+            circle(borderx + (frame*(float)7.5),(bordery + height-(bordery*2)) + (detail*3) + (frame),frame/2);//line in
+            stroke(115, 199, 132);//light green
+            circle(borderx + (frame*(float)7.5) + frame,(bordery + height-(bordery*2)) + (detail*3) + (frame),frame/2);//line out
+        }//end if statement
+        //tv screen
+        noStroke();
+        fill(20);
+        rect(borderx, bordery, width-(borderx*2), height-(bordery*2), detail*2);
+    }//end draw
 
-        rot += getAmplitude() / 8.0f;
-
-        rotateY(rot);
-        float[] bands = getSmoothedBands();
-        for(int i = 0 ; i < bands.length ; i ++)
-        {
-            float theta = map(i, 0, bands.length, 0, TWO_PI);
-
-            stroke(map(i, 0, bands.length, 0, 255), 255, 255);
-            float x = sin(theta) * radius;
-            float z = cos(theta) * radius;
-            float h = bands[i];
-            pushMatrix();
-            translate(x, - h / 2 , z);
-            rotateY(theta);
-            box(50, h, 50);
-            popMatrix();
-        }
-
-    }
-    float angle = 0;
-
-}
+}//end NameVisual
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,10 +1,13 @@
 package ie.tudublin;
 
+/*
 import example.AudioBandsVisual;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
+*/
 import c123456.BryansVisual;
+
 public class Main
 {	
 
@@ -17,6 +20,6 @@ public class Main
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();		
 	}
 }
