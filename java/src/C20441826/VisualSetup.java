c961d76a056371fed81a1fd4bbf0f30e8a11fc69
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-04-29 20:20:29 +0100

    Cleaned up files more, added TV buttons, fixed visual problems

--- java/src/C20441826/VisualSetup.java
+++ java/src/C20441826/VisualSetup.java
@@ -1,7 +1,6 @@
 package C20441826;
 
 import ie.tudublin.Visual;
-import ie.tudublin.VisualException;
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
@@ -11,46 +10,51 @@ public class VisualSetup extends Visual
 {
 
     //declare variables
+    //main
     Minim minim;
     AudioPlayer ap;
     AudioInput ai;
     AudioBuffer ab;
-    int x, j;
-    int mode = 1;
+    //audio response
     float lerpedBuffer[];
-    float y = 0;
-    float smoothedY = 0;
-    float smoothedAmplitude = 0;
-    float borderx;
-    float bordery;
-    float halfH;
-    float halfW;
+    //border calculation
+    float borderx = width * 0.2f;
+    float bordery = height * 0.25f;
+    float frame = 30f;
+    float detail = 5f;
+    float halfH = height / 2;
+    float halfW = width/2;
+    //declare visuals
     Viz1 viz1;
     Viz2 viz2;
     Viz3 viz3;
     Viz4 viz4;
     VisualTV tv;
-    float n4;
+    //visual mode
+    int mode = 1;
     
     //settings
     public void settings()
     {
-        size(700,700, P3D);//min size for tv graphic is 500 x 500
+        size(500,500, P3D);//min size for tv graphic is 500 x 500
         println("CWD: " + System.getProperty("user.dir"));//current working directory
-        //fullScreen(P3D,SPAN);// full screen
+        fullScreen(P3D,SPAN);// full screen
     }//end settings
 
     //check for key press
     public void keyPressed() 
     {
+
         //for choosing visualizations
 		if (key >= '1' && key <= '6') 
         {
 			mode = key - '0';
 		}//end if statement
+
         //spacebar for pausing
 		if (keyCode == ' ') 
         {
+
             //if playing
             if (ap.isPlaying()) 
             {
@@ -64,39 +68,43 @@ public class VisualSetup extends Visual
                 ap.rewind();
                 ap.play();
             }//end else statement
+
         }//end if statement
+
 	}//end key pressed
 
     //setup
     public void setup()
     {
-        minim = new Minim(this);
+
+        //color mode
         colorMode(HSB);
-        setFrameSize(256);
-        startMinim();
-        //startListening(); 
+        //begin visualization
+        minim = new Minim(this);
         ap = minim.loadFile("rapgod.mp3", height+width);
-        //ap.setGain(-10); // set volume
         ap.play();
         ab = ap.mix;
-        y = height / 2;
-        smoothedY = y;
         lerpedBuffer = new float[width];
         //declare visualizations
-        viz1 = new Viz1(width, height, this);
+        viz1 = new Viz1(width, height, lerpedBuffer, this);
         viz2 = new Viz2(width, height, this);
-        viz3 = new Viz3(width, height, smoothedAmplitude, lerpedBuffer, this);
-        viz4 = new Viz4(width, height, smoothedAmplitude, lerpedBuffer, this);
+        viz3 = new Viz3(width, height, lerpedBuffer, this);
+        viz4 = new Viz4(width, height, lerpedBuffer, this);
         tv = new VisualTV(width, height, this);
+
     }//end setup
 
     //draw
     public void draw()
     {
+
+        //jeff
         tv.render();
+
         //switching visualizations
         switch (mode) 
         {
+
             //first visualization
             case 1:
             {
@@ -126,38 +134,51 @@ public class VisualSetup extends Visual
             case 5:
             {
                 viz5.render();
+                break;
             }//end case 5
             //final visualization
             case 6:
             {
                 viz6.render();
+                break;
             }//end case 
             */
+
         }//end switch
+
     }//end draw
 
+    //mouse clicked
     public void mouseClicked()
     {   
-        //declare local variable
-        float detail = 5f;
+
+        //variables need to be in here
+        float borderx = width * 0.2f;
+        float bordery = height * 0.25f;
         float frame = 30f;
+        float detail = 5f;
 
         //first button --> control play and pause
         if (mouseX >= borderx + frame && mouseX <= borderx + frame + (frame*2) && mouseY >= ((bordery + height-(bordery*2)) + (detail*3) + (frame / 2)) && mouseY <= ((bordery + height-(bordery*2)) + (detail*3) + (frame / 2)) + frame)
         {
+
+            //if playing
             if (ap.isPlaying())
             {
                 ap.pause();
             }//end if statement
+            //if not playing
             else
             {
                 ap.play();
             }//end else statement
+
         }//end if statement
 
         //second button --> previous visualization
         if(mouseX >= borderx + (frame*(float)2.5) + frame && mouseX <= borderx + (frame*(float)2.5) + frame + (frame - (frame/3))&& mouseY >= (bordery + height-(bordery*2)) + (detail*3) + (frame / 2) && mouseY <= (bordery + height-(bordery*2)) + (detail*3) + (frame / 2) + frame)
         {
+
             //first visualization
             if(mode == 1)
             {
@@ -167,32 +188,34 @@ public class VisualSetup extends Visual
             else if(mode == 2)
             {
                 mode = 1;
-            }//end if statement
+            }//end else if statement
             //third visualization
             else if(mode == 3)
             {
                 mode = 2;
-            }//end if statement
+            }//end else if statement
             //fourth visualization
             else if(mode == 4)
             {
                 mode = 3;
-            }//end if statement
+            }//end else if statement
             //fifth visualization
             else if(mode == 5)
             {
                 mode = 4;
-            }//end if statement
+            }//end else if statement
             //sixth visualization
             else if(mode == 6)
             {
                 mode = 5;
-            }//end if statement
+            }//end else if statement
+
         }//end if statement
 
         //third button --> next visualization
         if(mouseX >= borderx + (frame*(float)2.5) + (frame*2) && mouseX <= borderx + (frame*(float)2.5) + (frame*2) + (frame - (frame/3))&& mouseY >= (bordery + height-(bordery*2)) + (detail*3) + (frame / 2) && mouseY <= (bordery + height-(bordery*2)) + (detail*3) + (frame / 2) + frame)
         {
+
             //first visualization
             if(mode == 1)
             {
@@ -202,32 +225,30 @@ public class VisualSetup extends Visual
             else if(mode == 2)
             {
                 mode = 3;
-            }//end if statement
+            }//end else if statement
             //third visualization
             else if(mode == 3)
             {
                 mode = 4;
-            }//end if statement
+            }//end else if statement
             //fourth visualization
             else if(mode == 4)
             {
                 mode = 5;
-            }//end if statement
+            }//end else if statement
             //fifth visualization
             else if(mode == 5)
             {
                 mode = 6;
-            }//end if statement
+            }//end else if statement
             //sixth visualization
             else if(mode == 6)
             {
                 mode = 1;
-            }//end if statement
+            }//end  else if statement
 
         }//end if statement
 
     }//end mouseClicked
-
-    
 
 }//end NameVisual
--- java/src/C20441826/Viz1.java
+++ java/src/C20441826/Viz1.java
@@ -1,120 +1,119 @@
 package C20441826;
-import ie.tudublin.Visual;
-import processing.core.PApplet;
 
-public class Viz1 
+public class Viz1 extends VisualSetup
 {
     
+    //declare variables
     private float x, y;
     private VisualSetup viz;
     private float[] lerpedBufferX;
     private float[] lerpedBufferY;
-    
     private float width, height;
 
     //constructor for first visualizer
-    public Viz1(float width, float height, VisualSetup viz)
+    public Viz1(float width, float height, float lerpedBuffer[], VisualSetup viz)
     {
         this.height = height;
         this.width = width;
         this.viz = viz;
-        
-    }
+        this.lerpedBufferX = lerpedBuffer;
+        this.lerpedBufferY = lerpedBuffer;
+    }//end Viz1
 
     // render class called from draw
     public void render()
     {
-        // Calculate distance from sides of tv
+        
+        //calculate distance from sides of tv
         float borderx = width * 0.2f;
         float bordery = height * 0.25f;
         
-        // Calculate half of height
+        //calculate half of height
         float halfH = height/2;
 
-        lerpedBufferX = viz.lerpedBuffer;
-        lerpedBufferY = viz.lerpedBuffer;
-
-        // calculate lerped buffer for the visualizers going across the y axis of the screen
+        //calculate lerped buffer for the visualizers going across the y axis of the screen
         for(int i = (int)bordery ; i < height-bordery ; i++)
         {
-            lerpedBufferY[i] = PApplet.lerp(lerpedBufferY[i], viz.ab.get(i), 0.2f);
-        }
+            lerpedBufferY[i] = lerp(lerpedBufferY[i], viz.ab.get(i), 0.2f);
+        }//end for loop
         
-        // vizualizers at bottom and top of the screen including sporadic circles
+        //vizualizers at bottom and top of the screen including sporadic circles
         for (int i = (int)borderx ; i < width-borderx ; i ++)
         {
+
             //colour vizualizers
-            float c = PApplet.map(i, 0, width-borderx, 0, 255);
-            
+            float c = map(i, 0, width-borderx, 0, 255);
             //colour used for stroke of the line line vizualizers
             viz.stroke(c, 255, 255);
 
             // calculate lerped buffer for the visualizers going across the x axis of the screen
-            lerpedBufferX[i] = PApplet.lerp(lerpedBufferX[i], viz.ab.get(i), 0.2f);
-            
+            lerpedBufferX[i] = lerp(lerpedBufferX[i], viz.ab.get(i), 0.2f);
             //calculate frequency to be relative to the range from the top or bottom of the border to the screen
             float f = lerpedBufferX[i] * bordery;
 
-            // bottom visualizer, if statement filters out bottom of the bottom line vizualizer
+            //bottom visualizer, if statement filters out bottom of the bottom line vizualizer
             if ((halfH/2) + f + (bordery*2) < (halfH/2) - f + (bordery*2))
             {
                 viz.line(i, (halfH/2) + (f/2) + (bordery*2), i, (halfH/2) + (bordery*2));  
-            }
-            // top visualizer, if statement filters out top of the line top vizualizer
+            }//end if statement
+
+            //top visualizer, if statement filters out top of the line top vizualizer
             if ((halfH/2) + f > (halfH/2) - f)
             {
                 viz.line(i, (halfH/2) + (f/2), i, (halfH/2));
-            }
+            }//end if statement
 
             // calculate x and y to spread of ellipses
-            x = PApplet.sin(i)*5;
-            y = PApplet.sin(i)*width-(borderx*2);
+            x = sin(i)*5;
+            y = sin(i)*width-(borderx*2);
 
             //filter out remaining circles outside of tv
-            if ( i + x > borderx + 10 && i+x < width-borderx - 10 && i+y > bordery + 10 && i+y < height - bordery - 10)
+            if ( i + x > borderx + 20 && i+x < width-borderx - 20 && i+y > bordery + 20 && i+y < height - bordery - 20)
             {
                 viz.ellipse(i + x, i + y, f/3, f/3);
-            }
-            
-        }
-        
+            }//end if statement  
+
+        }//end for loop
+
         // iterate through the width of the screen - 20 on both sides so visualizer doesnt merge with side visualizers
         for(int i = (int)borderx ; i < width-(borderx*2); i ++)
         {
-            // colour for main vizualizer
-            float c = PApplet.map(i, 0, width-borderx, 255, 0);
-            viz.stroke(c, 255, 255);
 
+            // colour for main vizualizer
+            float c = map(i, 0, width-borderx, 255, 0);
+            viz.stroke(c, 255, 255);
             //calculate frequency to be relative to the range from the bottom or top of the border to the screen
             float f = lerpedBufferX[i] * bordery;
-
             // main vizualizer in the center
-            viz.line(i+10, (halfH) + (f), width-10- (i), (halfH)-(f) );
-        }
-        
+            viz.line(i+10, (halfH) + (f), width-10- (i), (halfH)-(f));
+
+        }//end for loop
+
         // iterate through the length of the screen, vizualizers on left and right hand side of the screen
         for(int i = (int)bordery; i < height -bordery; i ++)
         {
+
             // colour for left and right vizualizers
-            float c = PApplet.map(i, 0, height-bordery, 0, 255);
+            float c = map(i, 0, height-bordery, 0, 255);
             viz.stroke(c, 255, 255);
 
-            // calculate frequency to be relative to the range from the bottom or top of the border to the screen
+            //calculate frequency to be relative to the range from the bottom or top of the border to the screen
             float f = lerpedBufferY[i] * bordery;
-            
-            // left visualizer, if statement filters out left side of the left line vizualizer
+
+            //left visualizer, if statement filters out left side of the left line vizualizer
             if (f + (borderx*4) > borderx*4)
             {
                 viz.line(borderx, i, f/2 + borderx, i);
-            }
-            // right visualizer, if statement filters out right side of the right line vizualizer
+            }//end if statement
+
+            //right visualizer, if statement filters out right side of the right line vizualizer
             if (f + (borderx*4) < borderx*4)
             {
                 viz.line(borderx*4, i, f/2 + (borderx*4), i);
-            }
-            
-        }
+            }//end if statement
 
-    }
+        }//end for loop
+
+    }//end render
     
-}
+}//end class
